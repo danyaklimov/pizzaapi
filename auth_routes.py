4d27493b0ef3ab6bf42c21c08840bc465e9c5f64
Danya <daklimovmath@mail.ru>
2023-06-15 23:33:28 +0300

    protected path /auth/, added /auth/refresh route for refreshing jwt token

--- auth_routes.py
+++ auth_routes.py
@@ -17,7 +17,12 @@ session = Session(bind=engine)
 
 
 @auth_router.get('/')
-async def hello():
+async def hello(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
     return {"message": "Hello world"}
 
 
@@ -62,3 +67,17 @@ async def login(user: LoginModel, Authorize: AuthJWT = Depends()):
         return jsonable_encoder(response)
 
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid username or password")
+
+@auth_router.get('/refresh')
+async def refresh_token(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_refresh_token_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Provide a valid refresh token")
+
+    current_user = Authorize.get_jwt_subject()
+    access_token = Authorize.create_access_token(subject=current_user)
+
+    response = {"access": access_token}
+
+    return jsonable_encoder(response)
