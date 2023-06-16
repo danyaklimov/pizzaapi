be345d4e7b50b45879288fbd2114740598272e85
Danya <daklimovmath@mail.ru>
2023-06-17 00:13:39 +0300

    created OrderModel class for validation orders, created post route for placing order in db

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from fastapi_jwt_auth import AuthJWT
+
+from database import engine, Session
+from models import User, Order
+from schemas import OrderModel
+
+session = Session(bind=engine)
 
 order_router = APIRouter(
     prefix="/orders",
@@ -6,5 +14,37 @@ order_router = APIRouter(
 )
 
 @order_router.get('/')
-async def hello():
+async def hello(Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
     return {"message": "Hello world"}
+
+@order_router.post('/order', status_code=status.HTTP_201_CREATED)
+async def place_an_order(order: OrderModel, Authorize: AuthJWT = Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+    current_user = Authorize.get_jwt_subject()
+    user = session.query(User).filter(User.username == current_user).first()
+    new_order = Order(
+        pizza_size=order.pizza_size,
+        quantity=order.quantity
+    )
+    new_order.user = user
+
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "pizza_size": new_order.pizza_size,
+        "quantity": new_order.quantity,
+        "id": new_order.id,
+        "user": new_order.user
+    }
+
+    return jsonable_encoder(response)
